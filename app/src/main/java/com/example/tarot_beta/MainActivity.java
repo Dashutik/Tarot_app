3722833da8c19221905a22a3ce3da201bb352141
aklana <mashachircova2@gmail.com>
2025-03-16 23:06:53 +0500

    кнопка и рандом

--- app/src/main/java/com/example/tarot_beta/MainActivity.java
+++ app/src/main/java/com/example/tarot_beta/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.tarot_beta;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView cardImageView;
 
+    private final int[] tarotCards = {
+            R.drawable.coins01, R.drawable.coins02, R.drawable.coins03,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cardImageView = findViewById(R.id.cardImageView);
+        Button pickCardButton = findViewById(R.id.button5);
+
+        String[] tarotCardNames = getResources().getStringArray(R.array.tarot_cards);
+
+        pickCardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int randomIndex = (int) (Math.random() * tarotCards.length);
+                cardImageView.setImageResource(tarotCards[randomIndex]);
+
+                Toast.makeText(MainActivity.this, tarotCardNames[randomIndex], Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
