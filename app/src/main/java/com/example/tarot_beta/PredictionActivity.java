deef77851ddecf0a422b4128a70a724baa904855
aklana <mashachircova2@gmail.com>
2025-04-07 19:37:52 +0500

    все карты и их описание

--- app/src/main/java/com/example/tarot_beta/PredictionActivity.java
+++ app/src/main/java/com/example/tarot_beta/PredictionActivity.java
@@ -1,6 +1,7 @@
 package com.example.tarot_beta;
 
 import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,14 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Toast;
+
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class PredictionActivity extends AppCompatActivity {
     private ImageView cardImageView;
-
-    private final int[] tarotCards = {
-            R.drawable.coins01, R.drawable.coins02, R.drawable.coins03,
-    };
+    private TextView cardNameTextView, cardDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +30,26 @@ public class PredictionActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        cardImageView = findViewById(R.id.cardImageView);
-        String[] tarotCardNames = getResources().getStringArray(R.array.tarot_cards);
-        int randomIndex = (int) (Math.random() * tarotCards.length);
-        cardImageView.setImageResource(tarotCards[randomIndex]);
+        showRandomTarotCard();
 
+    }
+    private void showRandomTarotCard() {
+        TypedArray cardImages = getResources().obtainTypedArray(R.array.tarot_card_images);
+
+        String[] cardNames = getResources().getStringArray(R.array.tarot_card_names);
+        String[] cardDescriptions = getResources().getStringArray(R.array.tarot_card_descriptions);
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(cardImages.length());
+
+        cardImageView = findViewById(R.id.cardImageView);
+        cardNameTextView = findViewById(R.id.cardNameTextView);
+        cardDescriptionTextView = findViewById(R.id.cardDescriptionTextView);
+
+        int imageResId = cardImages.getResourceId(randomIndex, -1);
+        cardImageView.setImageResource(imageResId);
+        cardNameTextView.setText(cardNames[randomIndex]);
+        cardDescriptionTextView.setText(cardDescriptions[randomIndex]);
     }
     public void backToMain(View v) {
         Intent intent = new Intent(PredictionActivity.this, MainActivity.class);
